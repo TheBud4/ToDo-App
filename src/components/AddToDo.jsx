97a5a0e89f486f09c8b17d82e58eeacfe18f82f0
Murilo Pistore <murilopmr13@hotmail.com>
2023-10-05 14:32:54 -0300

    addToDo changes

--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -1,9 +1,46 @@
+import plusIcon from "../assets/plus_icon.svg";
+import { useState, useEffect } from "react";
 
+function YourComponent() {
+  const [open, setOpen] = useState(false);
 
-export const AddToDo = () => {
+  useEffect(() => {
+    if (open) {
+      const Background = document.getElementById("bg");
+      Background.addEventListener("click", closeAdd);
+    } else {
+      const Background = document.getElementById("bg");
+      Background.removeEventListener("click", closeAdd);
+    }
+
+    return () => {
+      const Background = document.getElementById("bg");
+      Background.removeEventListener("click", closeAdd);
+    };
+  }, [open]);
+
+  const openAdd = () => {
+    setOpen(true);
+  };
+
+  function closeAdd() {
+    setOpen(false);
+  }
+
   return (
-    <button><span className="material-symbols-outlined">
-    </span></button>
-  )
+    <>
+      <button className="addToDo" onClick={openAdd}>
+        <img src={plusIcon} alt="Add" />
+      </button>
+      {open ? (
+        <div className="Background" id="bg">
+          <div className="modalToDo">
+            <form>{/* Your form content here */}</form>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
 }
-export default AddToDo;
+
+export default YourComponent;
