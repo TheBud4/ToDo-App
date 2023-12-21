121a069e7a4af4479f1e51d2d07745e46bdbe73d
Murilo Pistore <murilopmr13@hotmail.com>
2023-12-20 22:09:22 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import "./App.css";
 import AddToDo from "./components/AddToDo";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [Savedtodos, setSavedTodos] = useState([]);
+
+  useEffect(() => {
+    // Fetch todos from localStorage on component mount
+    const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    setSavedTodos(savedTodos);
+  }, []);
+
   return (
     <>
       <header>
-      <h1>Lista de Afazeres</h1>
-      <AddToDo/>
+        <h1>Lista de Afazeres</h1>
+        <AddToDo />
       </header>
       <div className="Divider"></div>
       <div className="Container">
-
+        <ul>
+          {Savedtodos.map((todo, index) => (
+            <li key={index}>
+              <strong>Title:</strong> {todo.title} <br />
+              <strong>Description:</strong> {todo.description} <br />
+              <strong>Tag:</strong> {todo.tag}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -1,30 +1,49 @@
+
 import plusIcon from "../assets/plus_icon.svg";
-import { useState, useEffect } from "react";
+import xIcon from "../assets/closeIcon.svg";
+import { useState } from "react";
 
-function YourComponent() {
+export function AddToDo() {
   const [open, setOpen] = useState(false);
-
-  useEffect(() => {
-    if (open) {
-      const Background = document.getElementById("bg");
-      Background.addEventListener("click", closeAdd);
-    } else {
-      const Background = document.getElementById("bg");
-      Background.removeEventListener("click", closeAdd);
-    }
-
-    return () => {
-      const Background = document.getElementById("bg");
-      Background.removeEventListener("click", closeAdd);
-    };
-  }, [open]);
-
-  const openAdd = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    tag: "",
+  });
+  const [todos, setTodos] = useState([]);
+  function openAdd() {
     setOpen(true);
-  };
+  }
 
   function closeAdd() {
     setOpen(false);
+  }
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+
+  function manageToDo(event) {
+    event.preventDefault();
+
+    // Save the new todo to localStorage
+    const newTodo = {
+      title: formData.title,
+      description: formData.description,
+      tag: formData.tag,
+    };
+
+    const updatedTodos = [...todos, newTodo];
+    localStorage.setItem('todos', JSON.stringify(updatedTodos));
+
+    // Update state to display the new todo
+    setTodos(updatedTodos);
+
+    closeAdd();
   }
 
   return (
@@ -35,7 +54,53 @@ function YourComponent() {
       {open ? (
         <div className="Background" id="bg">
           <div className="modalToDo">
-            <form>{/* Your form content here */}</form>
+            <div className="rightAlign">
+              <button className="addToDo end" onClick={closeAdd}>
+                <img src={xIcon} alt="fechar" />
+              </button>
+            </div>
+            <form onSubmit={manageToDo}>
+              
+              <div className="form__group field">
+                <input
+                  type="input"
+                  className="form__field"
+                  placeholder="title"
+                  title="title"
+                  id="title"
+                  name="title"
+                  onChange={handleInputChange}
+                  required
+                />
+                <label htmlFor="title" className="form__label">
+                  Titulo
+                </label>
+              </div>
+              <span>Descrição:</span>
+              <textarea
+                rows="1"
+                name="description"
+                id="description"
+                onChange={handleInputChange}
+              />
+              <span>Tag: </span>
+              <div className="containerSmall">
+                <select
+                  name="tag"
+                  id="tag"
+                  onChange={handleInputChange}
+                >
+                  <option value="">Selecione Aqui a Tag</option>
+                  <option value="Trabalho">Trabalho</option>
+                  <option value="Estudo">Estudo</option>
+                  <option value="Treino">Treino</option>
+                  <option value="Lazer">Lazer</option>
+                </select>
+                <div className="rightAlign">
+                  <input type="submit" value="Adicionar" className="add" />
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       ) : null}
@@ -43,4 +108,4 @@ function YourComponent() {
   );
 }
 
-export default YourComponent;
+export default AddToDo;
