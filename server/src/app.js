7f6a0af3ad5f14894802735fffc2cdef3b1dfba3
Murilo Pistore <murilopmr13@hotmail.com>
2023-12-31 21:25:16 -0300

    .

---
+++ server/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Example app listening on port ${process.env.SERVER_PORT}`)
+})
