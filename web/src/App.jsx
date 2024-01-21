92b72008f0fa7446b99c91e380d1c434dc340c7d
Murilo Pistore <murilopmr13@hotmail.com>
2024-01-21 14:30:21 -0300

    atualizando o modal de ToDos

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -9,6 +9,15 @@ function App() {
   const [tag, setTag] = useState("");
   const [todos, setTodos] = useState([]);
 
+  function resetForm () {
+
+    setTitle("");
+    setDescription("");
+    setTag("");
+    closeAdd();
+
+  }
+
   const handleInputChange = (event) => {
     const { id, value } = event.target;
 
@@ -30,21 +39,18 @@ function App() {
   const manageToDo = (event) => {
     event.preventDefault();
 
-    // Create a new todo object
+    // Cria um novo objeto todo
     const newTodo = {
       title: title,
       description: description,
       tag: tag,
     };
 
-    // Update the todos state with the new todo
+    // Atualiza a lista de ToDos
     setTodos([...todos, newTodo]);
 
-    // Reset the form
-    setTitle("");
-    setDescription("");
-    setTag("");
-    closeAdd();
+    resetForm();
+
   };
 
   function openAdd() {
@@ -91,34 +97,60 @@ function App() {
             >
               <img src={xIcon} alt="fechar" />
             </button>
-            <form onSubmit={manageToDo} className="flex size-full flex-col justify-evenly ">
+            <form
+              onSubmit={manageToDo}
+              className="flex size-full flex-col justify-evenly "
+            >
               <h2>Escreva aqui sua tarefa</h2>
-              <input
-                type="text"
-                id="title"
-                placeholder="Titulo"
-                onChange={handleInputChange}
-              />
-              <textarea
-                rows="1"
-                id="description"
-                placeholder="Descrição"
-                onChange={handleInputChange}
-              />
-              <div className="containerSmall">
-                <select id="tag" onChange={handleInputChange}>
-                  <option value="">Selecione Aqui a Tag</option>
-                  <option value="Trabalho">Trabalho</option>
-                  <option value="Estudo">Estudo</option>
-                  <option value="Treino">Treino</option>
-                  <option value="Lazer">Lazer</option>
-                </select>
-                <div className="rightAlign">
-                  <input
+
+              <div className="relative h-10 w-full min-w-[200px]">
+                <input
+                  type="text"
+                  className="peer h-full w-full rounded-[7px] border border-white  bg-transparent px-3 py-2.5 font-sans text-sm font-normal  !text-white outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-white focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                  placeholder=" "
+                  id="title"
+                  onChange={handleInputChange}
+                />
+                <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight !text-white transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-white before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-white after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-white peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-white peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-white peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                  Titulo
+                </label>
+              </div>
+
+              <div className="relative w-full min-w-[200px]">
+                <textarea
+                  rows="8"
+                  className="peer h-full min-h-[100px] w-full !resize-none  rounded-[7px] border border-white bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-dark-textColor outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-white placeholder-shown:border-t-white focus:border-2 focus:border-white focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
+                  placeholder=" "
+                  id="description"
+                  onChange={handleInputChange}
+                ></textarea>
+                <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-dark-textColor transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-dark-primaryColor before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-dark-primaryColor after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-dark-primaryColor peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-dark-textColor peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-dark-textColor peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-dark-primaryColor peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-dark-textColor">
+                  Descrição
+                </label>
+              </div>
+              <div className="flex w-full justify-between py-1.5">
+                <button
+                  className="relative h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-blue-gray-500 transition-all hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                  type="button"
+                ></button>
+                <div className="flex gap-2">
+                  <select
+                    id="tag"
+                    onChange={handleInputChange}
+                    className="peer h-full w-full rounded-[7px] border border-dark-primaryColor bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-dark-textColor outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-dark-primaryColor empty:!bg-dark-primaryColor focus:border-2 focus:border-dark-primaryColor focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                  >
+                    <option value="">Selecione Aqui a Tag</option>
+                    <option value="Trabalho">Trabalho</option>
+                    <option value="Estudo">Estudo</option>
+                    <option value="Treino">Treino</option>
+                    <option value="Lazer">Lazer</option>
+                  </select>
+                  <button
+                    className="px-4  font-sans text-xs font-bold text-center text-dark-textColor uppercase align-middle transition-all rounded-md select-none hover:bg-dark-primaryColor hover:text-dark-modalBgColor active:bg-dark-primaryColor disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                     type="submit"
-                    value="Adicionar"
-                    className="bg-dark-primaryColor text-dark-backGround"
-                  />
+                  >
+                    Adicionar
+                  </button>
                 </div>
               </div>
             </form>
