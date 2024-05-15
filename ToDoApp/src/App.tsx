a3f9f2f65e4890093901c97f51a45c2d49788433
Murilo Pistore <91626828+TheBud4@users.noreply.github.com>
2024-05-14 22:55:12 -0300

    começando com o frontend, adicionando fontes e ajeitando o layout principal

--- ToDoApp/src/App.tsx
+++ ToDoApp/src/App.tsx
@@ -1,10 +1,16 @@
-import '../styles/global.css';
+import "../styles/global.css";
 
 function App() {
-
   return (
-    <div>
-
+    <div className="flex flex-row h-screen">
+      <div className="h-full w-56 bg-zinc-700">
+        <h1 className="text-4xl text-center mt-14">Hello, World!</h1>
+      </div>
+      <div className="h-full p-16">
+        <h1 className="text-4xl text-center mt-14">
+          Veja Aqui as suas tarefas!
+        </h1>
+      </div>
     </div>
   );
 }
--- ToDoApp/tailwind.config.js
+++ ToDoApp/tailwind.config.js
@@ -4,20 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        dark: {
-          primary: '#292A29',
-          secondary: '#1F2E30',
-          tertiary: '#27181F',
-          quaternary: '#332521',
-          quinary: '#292523',
-        },
-        light: {
-          primary: '#FFEEDB',
-          secondary: '#61C9A8',
-          tertiary: '#BA3B46',
-          quaternary: '#ED9B40',
-          quinary: '#AA8F66',
-        },
+        
       },
     },
   },
