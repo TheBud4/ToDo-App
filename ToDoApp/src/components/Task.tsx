1102bc97e86ae33fce3b98efd3ab2927a34435df
Murilo Pistore <91626828+TheBud4@users.noreply.github.com>
2024-05-18 17:03:17 -0300

    feat: Update Task component layout and add date fields

--- ToDoApp/src/components/Task.tsx
+++ ToDoApp/src/components/Task.tsx
@@ -3,9 +3,15 @@ import { PencilSimple, Trash } from "@phosphor-icons/react";
 const Task = () => {
   return (
     <div className="flex flex-row justify-between py-8 px-16 border-t border-zinc-500">
-      <div className="flex flex-col">
-        <h1 className="">Title</h1>
-        <p className=""> Description</p>
+      <div className="flex flex-row w-full justify-between">
+        <div className="flex flex-col">
+          <h1 className="">titulo</h1>
+          <p className="">descricao</p>
+        </div>
+        <div className="flex flex-col mr-16">
+          <p>dataInicio</p>
+          <p>dataFim</p>
+        </div>
       </div>
       <div className="flex ">
         <button className="mr-4">
