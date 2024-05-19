2a595f36fad2c784cc295edca86b0aad5475b062
Murilo Pistore <91626828+TheBud4@users.noreply.github.com>
2024-05-19 15:25:09 -0300

    chore: Update npm dependencies and add CORS package

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,30 @@
-import express, { Request, Response } from 'express';
-import * as dotenv from 'dotenv';
+import express, { Request, Response } from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
-import userRoutes from './routes/UserRoute';
-import taskRoutes from './routes/TaskRoute';
+import userRoutes from "./routes/UserRoute";
+import taskRoutes from "./routes/TaskRoute";
 
 dotenv.config();
 
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 204,
+};
+
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
+app.use("/users", userRoutes);
 
-app.use('/users', userRoutes);
-app.use('/tasks', taskRoutes);
+app.use("/tasks", taskRoutes);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, world!');
-  });
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, world!");
+});
 
 app.listen(process.env.PORT_NUMBER, () => {
-    console.log(`Server on na porta ${process.env.PORT_NUMBER}`);
-  });
+  console.log(`Server on na porta ${process.env.PORT_NUMBER}`);
+});
